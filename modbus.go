ff6e25723c2da2879c5d32087f83bb56c8dd640a
Alexander Svensson <gurkslask@gmail.com>
2015-12-08 08:38:57 +0100

    Finished parsing os.argvs and am ready to make a connection

--- modbus.go
+++ modbus.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
+	"strconv"
+	//"strings"
 )
 
 func main() {
@@ -15,9 +18,26 @@ func main() {
 	if len(os.Args) < 4 {
 		panic("not enough parameters!")
 	}
-	ipadress := os.Args[1]
-	slaveid := os.Args[2]
-	startadress := os.Args[3]
-	num_of_registers := os.Args[4]
-	fmt.Printf("Ipadress: %v \nSlaveid; %v \nStartadress: %v\nNumber of registers: %v\n", ipadress, slaveid, startadress, num_of_registers)
+	var ipadress4 net.IP = net.ParseIP(os.Args[1])
+	if ipadress4 == nil {
+		panic("wrong ipadress format")
+	}
+	slaveid, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println(err)
+	}
+	startadress, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Println(err)
+	}
+	num_of_registers, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Ipadress: %v \nSlaveid; %v \nStartadress: %v\nNumber of registers: %v\n", ipadress4, slaveid, startadress, num_of_registers)
+
+	var TCPAdress net.TCPAddr
+	TCPAdress.IP = ipadress4
+	TCPAdress.Port = 502
+	fmt.Println(TCPAdress)
 }
